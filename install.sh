daaff62fb6a6b7d44a4aa00b82750046d823ec81
TheWatcherMultiversal <thewatcherjudge@protonmail.com>
2022-08-04 18:29:50 -0500

    We update the ./install.sh

--- install.sh
+++ install.sh
@@ -52,5 +52,45 @@ case "$PRIVILEGES" in
     esac
     ;;
 *)
-    echo "Error: Run this command with sudo"
+    case "$1" in
+    -u)
+        echo ""; echo -e "\e[1;36m   ///////     /////      ////    //  //////    //    //          ////////  ///////     ////////  ////    //\e[0m"
+        echo -e "\e[1:36m  //          //  //     // //   //  //    //  //    //             //     //          //    //  // //   //"
+        echo -e "\e[1:36m //          ///////    //  //  //  //    //  ////////  //////     //     //          //    //  //  //  //"
+        echo -e "\e[1;35m//          //    //   //   // //  //    //        //             //     //          //    //  //   // //"
+        echo -e "/////////  //     //  //    ////  ////////  ////////          ////////   /////////  ////////  //    ////\e[0m"; echo ""
+        echo -e "\e[1;32mGithub: \e[1;36mhttps://github.com/EliverLara/candy-icons\e[0m"
+        echo -e "\e[1;32mLicense: \e[1;36mGNU GENERAL PUBLIC LICENSE Version 3\e[0m"; echo ""
+
+        # Removing candy-icons
+        sleep 2s
+        echo "Removing candy-icons..."
+        rm -r $HOME/.local/share/icons/candy-icons-master/ && echo -e "\e[1;32mCandy-icon removed successfully\e[0m"
+        echo ""
+        ;;
+    *)
+        echo ""; echo -e "\e[1;36m   ///////     /////      ////    //  //////    //    //          ////////  ///////     ////////  ////    //\e[0m"
+        echo -e "\e[1:36m  //          //  //     // //   //  //    //  //    //             //     //          //    //  // //   //"
+        echo -e "\e[1:36m //          ///////    //  //  //  //    //  ////////  //////     //     //          //    //  //  //  //"
+        echo -e "\e[1;35m//          //    //   //   // //  //    //        //             //     //          //    //  //   // //"
+        echo -e "/////////  //     //  //    ////  ////////  ////////          ////////   /////////  ////////  //    ////\e[0m"; echo ""
+        echo -e "\e[1;32mGithub: \e[1;36mhttps://github.com/EliverLara/candy-icons\e[0m"
+        echo -e "\e[1;32mLicense: \e[1;36mGNU GENERAL PUBLIC LICENSE Version 3\e[0m"; echo ""
+    
+        # Extracting files
+        sleep 2s
+        echo "Extracting files..."
+        tar -xjf ./Install/candy-icons-master.tar.gz
+
+        # Copying candy-icons file
+        sleep 2s
+        echo "Candy-icons theme installation..."
+        cp -r candy-icons-master/ $HOME/.local/share/icons/
+        sleep 2s
+
+        # Removing traces
+        rm -r candy-icons-master/
+
+        echo''; echo -e "\e[1;32mCandy-icon installed successfully\e[0m"; echo ''
+        esac
 esac
